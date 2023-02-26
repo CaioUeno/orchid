859172f8a884d90de2f65a5829ac93cbb39f60a7
Caio Ueno <caioluiggy@hotmail.com>
2023-02-26 20:07:15 -0300

    add timer decorator

---
+++ timer.py
@@ -0,0 +1,45 @@
+from time import perf_counter, sleep
+from typing import Any, Callable
+from datetime import timedelta
+import logging
+
+
+def timer(logger: logging.Logger = None, level: str = "INFO") -> Callable:
+    """
+    Calculate function's run time.
+
+    Parameters
+    ----------
+        logger (logging.Logger) : logger object to log the run time as a message;
+        level (str) : logger level to use when logger parameter is given.
+    """
+
+    def wrapper(func: Callable[..., Any]) -> Callable:
+        def inner(*args, **kwargs) -> Any:
+            start = perf_counter()
+            r = func(*args, **kwargs)
+            end = perf_counter()
+            run_time = end - start
+
+            # remove microseconds
+            message = str(timedelta(seconds=run_time)).split(".")[0]
+
+            if logger is None:
+                print(message)
+            else:
+                logger.log(getattr(logging, level), message)
+
+            return r
+
+        return inner
+
+    return wrapper
+
+
+@timer(logger=None)
+def example(seconds: int = 3):
+    sleep(seconds)
+
+
+example()
+example(61)
