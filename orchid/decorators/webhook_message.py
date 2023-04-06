d0f89b22a006e1a53e456cf20e50b7db1ea7a02a
Caio Ueno <caioluiggy@hotmail.com>
2023-04-05 21:08:53 -0300

    finish webhook messenger decorator

--- orchid/decorators/webhook_message.py
+++ orchid/decorators/webhook_message.py
@@ -1,10 +1,9 @@
-import json
 from datetime import datetime
-from time import sleep
 from typing import Any, Callable, Union
 
 from discord_webhook import DiscordWebhook
-from slack_sdk.webhook import WebhookClient
+from requests import Response
+from slack_sdk.webhook import WebhookClient, WebhookResponse
 
 # +----------------------------------------------------------------------------------------------------------+
 # |                                                  SLACK                                                   |
@@ -23,146 +22,121 @@ from slack_sdk.webhook import WebhookClient
 # +--------------------------------------------------------------------------------------------------------------------------+
 
 
-def get_userid(app: str) -> str:
-    """Utils function"""
-    with open("webhooks.json") as file:
-        webhooks = json.load(file)
-        try:
-            return webhooks[app]["userid"]
-        except KeyError:
-            raise ValueError(
-                f"Unknown app name: {app}. Must be among {{`slack`, `discord`}}."
-            )
-
-
-def get_webhook(app: str, name: str) -> Union[WebhookClient, DiscordWebhook]:
-    """Utils function"""
-    wb_mapping = {"slack": WebhookClient, "discord": DiscordWebhook}
-    with open("webhooks.json") as file:
-        webhooks = json.load(file)
-
-        try:
-            for wb in webhooks[app]["webhooks"]:
-                if name == wb["name"]:
-                    url = wb["url"]
-                    return wb_mapping[app](url=url)
-
-            raise ValueError(f"Webhook name for {app} was not found: {name}")
-
-        except KeyError:
-            raise ValueError(
-                f"Unknown app name: {app}. Must be among {{`slack`, `discord`}}."
-            )
-
-
-def send_message(
-    message: str,
+def hermes(
     webhook: Union[WebhookClient, DiscordWebhook],
     tag: str = None,
     timestamp: bool = False,
-) -> bool:
-    """
-    Sends a message to the channel as a simple text.
-
-    Arguments
-    ---------
-        message (str): message to display on channel;
-        webhook (Union[WebhookClient, DiscordWebhook]): app webhook;
-        tag (str) : If not `None`, send a message tagging a user. Defaults to `None`;
-        timestamp (bool) : Whether to display timestamp information at the moment of the request. Defaults to False.
-    Returns:
-        bool : whether `response status` == 200 or not.
-    """
-
-    text = message
-
-    if tag is not None:
-        text = f"<@{tag}> - " + text
-
-    if timestamp:
-        text += f" ({datetime.now().replace(microsecond=0)})"
-
-    if isinstance(webhook, WebhookClient):
-        status_code = webhook.send(text=text).status_code
-
-    else:
-        webhook.set_content(content=text)
-        status_code = webhook.execute().status_code
-
-    return status_code == 200
-
-
-def hermes(app: str, name: str, tag: str = None, timestamp: bool = False) -> Callable:
+) -> Callable:
     """
     Decorator to encapsulate a function and `log` its behaviour/progress into an app channel.
 
     Arguments
     ---------
-        app (str) : app name among {`slack`, `discord`};
-        name (str) : webhook name;
-        tag (str) : If not `None`, send a message tagging a user (tag itself). Defaults to `None`;
-        timestamp (bool) : Whether to display timestamp information at the moment of the request. Defaults to `False`.
+        webhook : Union[WebhookClient, DiscordWebhook]
+            App webhook;
+        tag : str
+            If not `None`, send a message tagging a user (tag itself). Defaults to `None`;
+        timestamp : bool
+            Whether to display timestamp information at the moment of the request. Defaults to False.
     """
 
-    def wrapper(func: Callable) -> Callable:
-        webhook = get_webhook(app=app, name=name)
+    def format_message(
+        message: str,
+        tag: str = None,
+        timestamp: bool = False,
+    ) -> str:
+        """
+        Function to format a message. It may tag a user and/or add the timestamp.
 
+        Parameters
+        ----------
+            tag : str
+                If not `None`, send a message tagging a user (tag itself). Defaults to `None`;
+            timestamp : bool
+                Whether to display timestamp information at the moment of the request. Defaults to False.
+
+        Returns
+        -------
+            formatted_message : str
+                Formatted message.
+        """
+        formatted_message = message
+
+        if tag is not None:
+            formatted_message = f"<@{tag}> - " + formatted_message
+
+        if timestamp:
+            formatted_message += f" ({datetime.now().replace(microsecond=0)})"
+
+        return formatted_message
+
+    def send_message(
+        message: str,
+        webhook: Union[WebhookClient, DiscordWebhook],
+    ) -> Union[WebhookResponse, Response]:
+        """
+        Sends a message to the channel as a simple text.
+
+        Parameters
+        ---------
+            message : str
+                Message to send;
+            webhook : Union[WebhookClient, DiscordWebhook]
+                App webhook.
+
+        Returns
+        -------
+            status_code : int
+                reponse status code.
+        """
+
+        if isinstance(webhook, WebhookClient):
+            response = webhook.send(text=message)
+
+        else:
+            webhook.set_content(content=message)
+            response = webhook.execute()
+
+        return response
+
+    def wrapper(func: Callable) -> Callable:
         def inner(*args, **kwargs) -> Any:
             # before calling the function
             send_message(
-                message=f"Calling {func.__name__}",
+                message=format_message(
+                    f"Calling {func.__name__}", tag=tag, timestamp=timestamp
+                ),
                 webhook=webhook,
-                tag=tag,
-                timestamp=timestamp,
             )
 
             try:
                 r = func(*args, **kwargs)
+
+                # after the fuction was called
                 send_message(
-                    message=f"{func.__name__} has finished!",
+                    message=format_message(
+                        f"{func.__name__} has finished!", tag=tag, timestamp=timestamp
+                    ),
                     webhook=webhook,
-                    tag=tag,
-                    timestamp=timestamp,
                 )
 
                 return r
 
             except Exception as e:
+                # exception "warning"
                 send_message(
-                    message=f"{func.__name__} has failed!",
+                    message=format_message(
+                        f"{func.__name__} has failed!", tag=tag, timestamp=timestamp
+                    ),
                     webhook=webhook,
-                    tag=tag,
-                    timestamp=timestamp,
                 )
                 send_message(
                     message=str(e),
                     webhook=webhook,
-                    tag=tag,
-                    timestamp=timestamp,
                 )
 
-                return None
+                raise e
 
         return inner
 
     return wrapper
-
-
-# SLACK_USER_ID = get_userid("slack")
-# DISCORD_USER_ID = get_userid("discord")
-
-
-# @hermes(app="slack", name="clair", tag=SLACK_USER_ID, timestamp=True)
-# def example1(seconds: int = 2) -> None:
-#     sleep(seconds)
-
-
-# @hermes(app="discord", name="clair", tag=DISCORD_USER_ID, timestamp=True)
-# def example2(a: Any) -> None:
-#     # force an error
-#     raise ValueError(f"Raising an error!")
-
-
-# # calling decorated functions
-# example1(seconds=3)
-# example2(None)
--- tests/webhook_message/test_hermes.py
+++ tests/webhook_message/test_hermes.py
@@ -1,13 +1,64 @@
-from orchid import hermes
+import re
+from unittest.mock import Mock, patch
+
+from discord_webhook import DiscordWebhook
+from slack_sdk.webhook import WebhookClient
+
+from orchid.decorators import hermes
 
 
-# class TestHermes:
-#     def test_send_message_discord_no_meta(self):
-        
-#         @hermes
-#         def dummy():
-#             return True
+class TestHermes:
+    @staticmethod
+    def tmp_file_message(tmp_path, message):
+        d = tmp_path / "sub"
+        if not d.exists():
+            d.mkdir()
+        d
+        p = d / "tmp.txt"
+        p.write_text(message)
 
-#         returned = dummy()
+    # @patch("discord_webhook.DiscordWebhook", spec=DiscordWebhook)
+    # def test_send_message_discord(self, mock_webhook, tmp_path):
+    #     message = "a"
 
-#         assert returned
+    #     mock_webhook.set_content = Mock()
+
+    #     mock_webhook.execute = Mock(
+    #         side_effect=lambda: self.tmp_file_message(tmp_path, message)
+    #     )
+
+    #     @hermes(webhook=mock_webhook, tag="123")
+    #     def dummy():
+    #         return True
+
+    #     returned = dummy()
+
+    #     d = tmp_path / "sub"
+    #     p = d / "tmp.txt"
+
+    #     messsage_sent = p.read_text()
+
+    #     assert "<@123>" in messsage_sent
+
+    #     assert p.read_text() == "a"
+
+    @patch("slack_sdk.webhook.WebhookClient", spec=WebhookClient)
+    def test_send_message_slack(self, mock_webhook, tmp_path):
+        mock_webhook.send = Mock(
+            side_effect=lambda text: self.tmp_file_message(tmp_path, text)
+        )
+
+        @hermes(webhook=mock_webhook, tag="123", timestamp=True)
+        def dummy():
+            return True
+
+        _ = dummy()
+
+        d = tmp_path / "sub"
+        p = d / "tmp.txt"
+
+        messsage_sent = p.read_text()
+
+        assert "<@123>" in messsage_sent
+        assert "dummy has finished!" in messsage_sent
+        assert bool(re.search("\(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\)", messsage_sent))
